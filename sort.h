fdcbd13808a64c2b92db30f818ea27288c5dbb9f
Hyomin Kang <52210598+mango51@users.noreply.github.com>
2021-04-18 13:58:55 +0900

    0418 내가 해본 정렬 코드

--- sort.h
+++ sort.h
@@ -5,8 +5,11 @@
 
 #define SIZE		8
 
-void fnc_select_sort(int a[], int size);
-void fnc_bubble_sort(int a[], int size);
+void func_select_sort(int a[], int size);
+void func_select_print(int a[], int size);
 
-int fnc_qs_partition(int a[], int start, int end);
-void fnc_qs_sort(int a[], int start, int end);
+void func_bubble_sort(int a[], int size);
+void func_bubble_print(int a[], int size);
+
+int func_qs_partition(int a[], int start, int end);
+void func_qs_sort(int a[], int start, int end);
