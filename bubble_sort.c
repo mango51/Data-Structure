8c4f49d319f269b23830d827fbe6d201fb5ecd85
Hyomin Kang <52210598+mango51@users.noreply.github.com>
2021-04-18 13:59:55 +0900

    0418 버블소트

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,26 +1,22 @@
 #include "sort.h"
 
-void fnc_bubble_sort(int a[], int size){
-	int i, j, k, temp;
-
-	for(i=0; i<size; i++){
-		printf("%3d", a[i]);
-	}
-
-	printf("\n\n*******[Á¤·Ä]********\n");
-	for(i=size-1; i>0; i--){
-		for(j=0; j<i; j++){
-			if(a[j] > a[j+1]){
-				temp = a[j];
-				a[j] = a[j+1];
-				a[j+1] = temp;
-
-				for(k=0; k<size; k++){
-					printf("%3d", a[k]);
-				}
-				printf("\n");
-			}			
+void func_bubble_sort(int a[], int size){
+	int i,j;
+	int temp;
+	for(j=size-1;j>0;j--){
+		for(i=0;i<j;i++){
+			if(a[i] > a[i+1]){
+				temp = a[i];
+				a[i]= a[i+1];
+				a[i+1] = temp;
+			}
 		}
-		printf("\n");
+	}
+}
+
+void func_bubble_print(int a[], int size){
+	int i;
+	for(i=0;i<size;i++){
+			printf("a[%d] = %d\n",i,a[i]);
 	}
 }
